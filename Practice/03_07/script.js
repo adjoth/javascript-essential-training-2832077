33da371bde1e5ae15012b6d00e4813a052935a32
Adam Thomas <thomasadam911@yahoo.com>
2025-11-04 16:20:05 -0600

    Completed chapter 3

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const myComputer = {
+  brand: "Dell",
+  model: "XPS 13",
+  year: 2021,
+  onState: false,
+  togglePower: function () {
+    this.onState = !this.onState;
+  },
+};
+
+console.log("My computer object:", myComputer);
--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,37 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Backpack {
+  constructor(
+    // Defines parameters:
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen
+  ) {
+    // Define properties:
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength = {
+      left: strapLengthL,
+      right: strapLengthR,
+    };
+    this.lidOpen = lidOpen;
+  }
+  // Add methods like normal functions:
+  toggleLid(lidStatus) {
+    this.lidOpen = lidStatus;
+  }
+  newStrapLength(lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  }
+}
+
+export default Backpack;
