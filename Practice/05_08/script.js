77b1ec190004e2bf2807d44c36257e4a034d0d3a
Adam Thomas <thomasadam911@yahoo.com>
2025-11-04 21:10:34 -0600

    Completed chapter 5

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,14 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const backpack = document.querySelector("main .backpack");
+console.log(backpack);
+
+backpack.classList.add("new-class");
+backpack.classList.remove("new-class");
+backpack.dataset.test = "value";
+backpack.style.backgroundColor = "lightblue";
+
+const backpack2 = document.querySelector("main #pack02");
+console.log(backpack2);
