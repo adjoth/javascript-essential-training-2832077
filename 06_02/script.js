d800f284e8ddd30053f1e023f962a53088afe376
Adam Thomas <thomasadam911@yahoo.com>
2025-11-05 13:08:54 -0600

    Completed chapter 6

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,14 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+
+var container = 5;
+console.log(container);
+
+var x = 4,
+  y = 5,
+  z = "blue";
+console.log(x, y, z);
+
+var empty;
+console.log(empty);
--- 06_04/script.js
+++ 06_04/script.js
@@ -3,7 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-var color = "purple";
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
@@ -11,11 +11,15 @@ document.querySelector(".left .color-value").innerHTML = color;
 color = "skyblue";
 
 function headingColor() {
-  color = "blue";
-  document.querySelector(".title").style.color = color;
+  let titleColor = "blue"; // Block scope
+  document.querySelector(".title").style.color = titleColor;
+  console.log("Inside:", titleColor);
 }
 
 headingColor();
+
+// Line causes and error because titleColor is not in scope here
+// console.log("Inside:", titleColor);
 
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
